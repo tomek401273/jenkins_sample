3a86e3db45e4774135b6bc893e7f228d71eb2488
tomek401273 <tomek371240@gmail.com>
2019-04-19 09:54:18 +0200

    first-commit

---
+++ src/main/java/com/tgrajkowski/jenkinssample/MainController.java
@@ -0,0 +1,15 @@
+package com.tgrajkowski.jenkinssample;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class MainController {
+
+    @RequestMapping(value = "/logic")
+    public String logic() {
+        return "Logic logic logic";
+    }
+}
