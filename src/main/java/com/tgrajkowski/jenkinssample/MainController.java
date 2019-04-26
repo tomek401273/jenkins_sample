03961be155ba7b906e1a1e7f613118a6ca06daaa
tomek371240 <tomek371240@gmail.com>
2019-04-26 23:18:12 +0200

    logic6

--- src/main/java/com/tgrajkowski/jenkinssample/MainController.java
+++ src/main/java/com/tgrajkowski/jenkinssample/MainController.java
@@ -10,6 +10,6 @@ public class MainController {
 
     @RequestMapping(value = "/logic")
     public String logic() {
-        return "Logic logic logic5";
+        return "Logic logic logic6";
     }
 }
