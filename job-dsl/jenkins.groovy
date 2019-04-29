c7867cf2b86709ceb241efd3f3d38e04692a90c8
tomek401273 <tomek371240@gmail.com>
2019-04-29 17:26:41 +0200

    jenkins-groovy4

--- job-dsl/jenkins.groovy
+++ job-dsl/jenkins.groovy
@@ -13,8 +13,30 @@ job("seed-my") {
     concurrentBuild(false)
 
     steps {
-        println("This script will not work without a properly set up docker environment. Please define the environment variable 'DOCKER_HOST'")
-        shell("docker build -t tomek/jenkins:1 .")
-        shell("docker run -d -p 1000:8080 tomek/jenkins:1")
+//        shell("docker build -t tomek/jenkins:1 .")
+//        shell("docker run -d -p 1000:8080 tomek/jenkins:1")
+
+
+        def env = System.getenv()
+
+
+            println("This script will not work without a properly set up docker environment. Please define the environment variable 'DOCKER_HOST'")
+            false
+
+            runningDockers = "docker ps -q".execute().text.split(/\n/)
+            runningDockers.each {id -> println("docker stop ${id}".execute().text)}
+        println('end of message!!')
+
+//            stoppedDockers = "docker ps -a -f status=exited -q".execute().text.split(/\n/)
+//            stoppedDockers.each {id -> println("docker rm ${id}".execute().text)}
+//
+//            dockerImages = "docker images".execute().text.split(/\n/)
+//            snapshotImages = dockerImages.findAll{ it.contains("SNAPSHOT") || it.contains("<none>") }
+//            snapshotImages.each {output -> println("docker rmi ${output.split()[2]}".execute().text)}
+
+
+
+
+
     }
 }
